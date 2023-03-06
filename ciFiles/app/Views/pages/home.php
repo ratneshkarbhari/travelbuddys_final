0efbd50d2854b83a98efb69cd0bf2eb6affc6083
Ratnesh Karbhari <ratneshkarbhari18@gmail.com>
2023-03-06 15:41:46 +0530

    saving progress

--- ciFiles/app/Views/pages/home.php
+++ ciFiles/app/Views/pages/home.php
@@ -238,7 +238,7 @@
 
                         <div class="swiper-slide text-center trip-card">
                             <a href="<?php echo site_url('locations/' . $location->slug) ?>">
-                                <img class="trip-card-image lazy" src="<?php echo site_url('assets/images/placeholder.png') ?>" data-src="<?php echo site_url('assets/images/locations/' . $location->thumbnail) ?>">
+                                <img class="trip-card-image lazy" src="<?php echo site_url('assets/images/placeholder.png') ?>" data-src="<?php echo site_url('assets/images/locations/' . $location->featured_image) ?>">
                                 <h5 class="trip-title"><?php echo $location->title; ?></h5>
                             </a>
                         </div>
